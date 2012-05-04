d9022d7b3af5126cc64782512f1c810cd9c944e6
Loic Schmidt <loic.schmidt@inria.fr>
2012-05-04 15:29:29 +0200

    AdminUsers API and Admin Users management on web portal

--- admin_manageusers.php
+++ admin_manageusers.php
@@ -16,6 +16,7 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
         <div class="span12">
           <h2>Users</h2>
             <p></p>
+		<a href="#" class="btn btn-add" data-toggle="modal">Add user(s)</a>
                 <table id="tbl_users" class="table table-bordered table-striped table-condensed">
                 <thead>
                     <tr>
@@ -32,6 +33,89 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
         </div>
 
       </div>
+
+        <div id="add_modal" class="modal hide fade">
+            <div class="modal-header">
+              <a class="close" data-dismiss="modal">X</a>
+              <h3>Add user(s) <span id="s_login"></span></h3>
+              
+            </div>
+           <div class="modal-body">
+               <div class="alert alert-error" id="div_error" style="display:none"></div>
+               
+                <form class="well form-horizontal" id="form_add">
+
+              <div class="control-group">
+                <label class="control-label" for="txt_firstname">First Name:</label>
+                <div class="controls">
+                    <input id="txt_firstname" type="text" class="input-xlarge" required="required"/>
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_lastname">Last Name:</label>
+                <div class="controls">
+                    <input id="txt_lastname" type="text" class="input-xlarge" required="required"/>
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_howmany">How many user:</label>
+                <div class="controls">
+                    <input id="txt_howmany" type="text" class="input-xlarge" required="required" value="1"/>
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_email">Email:</label>
+                <div class="controls">
+                    <input id="txt_email" type="email" class="input-xlarge" required="required"/>
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_structure">Structure:</label>
+                <div class="controls">
+                    <input id="txt_structure" type="text" class="input-xlarge" required="required" />
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_city">City:</label>
+                <div class="controls">
+                    <input id="txt_city" type="text" class="input-xlarge" required="required" />
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_country">Country:</label>
+                <div class="controls">
+                    <input id="txt_country" type="text" class="input-xlarge" required="required" />
+                </div>
+              </div>
+
+              <div class="control-group">
+                <label class="control-label" for="txt_sshkey">SSH Key:</label>
+                <div class="controls">
+                    <textarea id="txt_sshkey" class="input-xlarge" rows="3" required="required"></textarea>
+                </div>
+              </div>
+              
+               <div class="control-group">
+                <label class="control-label" for="txt_motivation">Motivation:</label>
+                <div class="controls">
+                    <textarea id="txt_motivation" class="input-xlarge" rows="3" required="required"></textarea>
+                </div>
+              </div>
+              
+            </div>
+            
+            <div class="modal-footer">
+                   <button id="btn_add" class="btn btn-primary" type="submit">Add</button>
+                </form>
+            </div>
+            
+        </div>
 
         <div id="edit_modal" class="modal hide fade">
             <div class="modal-header">
@@ -121,7 +205,10 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
 
 <?php include('footer.php') ?>
 
-
+<link href="css/datatable.css" rel="stylesheet">
+<script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" language="javascript" src="js/datatable.js"></script>
+ 
     <script type="text/javascript">
 
     var users = {};
@@ -129,6 +216,7 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
 
     $(document).ready(function()
     {
+        $('#add_modal').modal('hide');
         $('#edit_modal').modal('hide');
 
         /* Load data in the table */
@@ -140,30 +228,34 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
                 users = data;
                 var i = 0;
                 $.each(data, function(key,val) {
-                    var btnState = "";
-                    var btnClass = "";
-                    if(val.validate) { btnState = 'disabled="true"' }
-                    else { btnClass="btn-primary" };
-                    
-                    if(val.admin) {
-                        btnIsAdminState = 'btn-warning';
-                    }
-                    else { 
-                        btnIsAdminState = '';
+                    var btnValidClass = "btn-primary";
+                    var btnValidValue="Pending";
+                    if(val.validate) {
+                         btnValidClass="";
+                         btnValidValue="Valid";
                     }
 
-                    //user row
+                    btnAdminClass = '';
+                    btnAdminValue = "User";
+                    if(val.admin) {
+                        btnAdminClass = 'btn-warning';
+                        btnAdminValue = "Admin";
+                    }
+ 
+                     //user row
                     $("#tbl_users tbody").append(
                     '<tr data=' + i + '>'+
                     '<td>' + val.login + '</td>'+
                     '<td>' + val.firstName + '</td>'+
                     '<td>'+ val.lastName +'</td>'+
                     '<td><a href="mailto:' + val.email + '">' + val.email + '</a></td>'+
-                    '<td><a href="#"><button class="btn ' + btnClass + ' validate " data="'+i+'" ' + btnState + 'onClick="validateUser('+i+')">Validate</button></a> ' +
-                    '<a href="#" class="btn btn-edit" data-toggle="modal" data="'+i+'">Edit</a> ' +
-                    '<a href="#" class="btn btn-admin '+btnIsAdminState+'" data="'+i+'" data-state="'+val.admin+'" onClick="setAdmin('+i+')">Admin</a> ' +
-                    '<a href="#"><button class="btn btn-danger" data="'+i+'" onClick="deleteUser('+i+')">Delete</button></a></td>'
+                    '<td><a href="#" class="btn btn-valid '+btnValidClass+'" data="'+i+'" data-state="'+val.validate+'" onClick="validateUser('+i+')">'+btnValidValue+'</a> ' +
+                        '<a href="#" class="btn btn-admin '+btnAdminClass+'" data="'+i+'" data-state="'+val.admin+'" onClick="setAdmin('+i+')">'+btnAdminValue+'</a> ' +
+                        '<a href="#" class="btn btn-edit" data-toggle="modal" data="'+i+'">Edit</a> ' +
+                        '<a href="#"><button class="btn btn-danger" data="'+i+'" onClick="deleteUser('+i+')">Delete</button></a></td>'
                     +'</tr>');
+                    $("tr[data="+i+"] .btn-valid").width(50);
+                    $("tr[data="+i+"] .btn-admin").width(50);
                     i++;
                 });
 
@@ -183,11 +275,81 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
                     $('#txt_motivation').val(selectedUser.motivations);
                     $("#edit_modal").modal('show');
                 });
+                $('#tbl_users').dataTable({
+			"sDom": "<'span8'f>t<'span8'i'p>",
+                        "bPaginate": true,
+                        "sPaginationType": "bootstrap",
+                        "bLengthChange": false,
+                        "bFilter": true,
+                        "bSort": true,
+                        "bInfo": true,
+                        "bAutoWidth": false
+                } );
+
             },
             error:function(XMLHttpRequest, textStatus, errorThrows){
                 alert("error: " + errorThrows)
             }
         });
+    });
+    
+    /* Add user(s) */
+    $(".btn-add").click(function(){
+	$("#add_modal").modal('show');
+    });
+    $('#form_add').bind('submit', function(){
+    
+        var userregister = {
+        "firstName":$("#txt_firstname").val(),
+        "lastName":$("#txt_lastname").val(),
+        "email":$("#txt_email").val(),
+        "structure":$("#txt_structure").val(),
+        "city":$("#txt_city").val(),
+        "country":$("#txt_country").val(),
+        "sshPublicKey":$("#txt_sshkey").val(),
+        "motivations":$("#txt_motivation").val(),
+        "nbUsersToAdd":$("#txt_howmany").val(),
+        };
+        
+        console.log(userregister);
+        
+        $.ajax({
+            url: "/rest/admin/users",
+            type: "POST",
+            dataType: "text",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(userregister),
+            success:function(data){
+                 window.location.reload();
+ 	    },
+            error:function(XMLHttpRequest, textStatus, errorThrows){
+                if(XMLHttpRequest.status == 409)
+                {
+                    $("#div_error").show();
+                    $("#div_error").removeClass("alert-success");
+                    $("#div_error").addClass("alert-error");
+                    $("#div_error").html("This user is already registered");
+                }
+                else if(XMLHttpRequest.status == 403)
+                {
+                    $("#div_error").show();
+                    $("#div_error").removeClass("alert-success");
+                    $("#div_error").addClass("alert-error");
+                    $("#div_error").html("Error");
+                    $("#cg_captcha").addClass("error");
+                    $("#captcha").focus();
+                }
+                else {
+                    $("#div_error").show();
+                    $("#div_error").removeClass("alert-success");
+                    $("#div_error").addClass("alert-error");
+                    $("#div_error").html(errorThrows); 
+                }  
+            }
+        });
+        
+    	return false;
+    
     });
     
     /* Delete a user */
@@ -213,24 +375,33 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
     };
     
     
-    /* Validate a user */
-    function validateUser(id) 
-    {
-        if(confirm("Validate user?"))
-        {
-            //validate field
-            var uservalidate = users[id];
-            uservalidate.validate = true;
+    /* Toggle Valid state */
+    function validateUser(id) {
+        var state = $("tr[data="+id+"] .btn-valid").attr("data-state");
+        var confirmText = "Validate user?";
+        if(state=="true") confirmText="Unvalidate user?";
+
+        if(confirm(confirmText)) {
+            //toggle validate flag
+            var user = users[id];
+            user.validate = !user.validate;
             
             $.ajax({
-                url: "/rest/admin/users/"+uservalidate.login,
+                url: "/rest/admin/users/"+user.login,
                 type: "PUT",
                 dataType: "text",
                 contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(uservalidate),
+                data: JSON.stringify(user),
                 success:function(data){
-                    $("tr[data="+id+"] .validate").attr("disabled","disabled")
-                    $("tr[data="+id+"] .validate").removeClass("btn-primary");
+                    if(state == "false") {
+                        $("tr[data="+id+"] .btn-valid").removeClass("btn-primary");
+                        $("tr[data="+id+"] .btn-valid").attr("data-state","true");
+                        $("tr[data="+id+"] .btn-valid").text("Valid");
+                    } else {
+                        $("tr[data="+id+"] .btn-valid").addClass("btn-primary");
+                        $("tr[data="+id+"] .btn-valid").attr("data-state","false");
+                        $("tr[data="+id+"] .btn-valid").text("Pending");
+                    }
                 },
                 error:function(XMLHttpRequest, textStatus, errorThrows){
                     alert("error:" + errorThrows)
@@ -238,6 +409,45 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
             })
         };
     };
+    
+    
+    /* Toggle Admin state */
+    function setAdmin(id) 
+    {
+        var state = $("tr[data="+id+"] .btn-admin").attr("data-state");
+        var confirmText = "Set Admin state?";
+        if(state=="true") confirmText="Unset Admin state?";
+
+
+        if(confirm(confirmText))
+        {
+            //toggle admin flag
+            var user = users[id];
+            user.admin = !user.admin;
+            
+            $.ajax({
+                url: "/rest/admin/users/"+user.login,
+                type: "PUT",
+                dataType: "text",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(user),
+                success:function(data){
+                    if(state == "false") {
+                        $("tr[data="+id+"] .btn-admin").addClass("btn-warning");
+                        $("tr[data="+id+"] .btn-admin").attr("data-state","true");
+                        $("tr[data="+id+"] .btn-admin").text("Admin");
+                    } else {
+                        $("tr[data="+id+"] .btn-admin").removeClass("btn-warning");
+                        $("tr[data="+id+"] .btn-admin").attr("data-state","false");
+                        $("tr[data="+id+"] .btn-admin").text("User");
+                    }
+                },
+                error:function(XMLHttpRequest, textStatus, errorThrows){
+                    alert("error:" + errorThrows)
+                }
+            })
+        };
+    };   
     
     
     /* Save values from edit modal */
@@ -274,43 +484,7 @@ if(!$_SESSION['is_auth'] || !$_SESSION['is_admin'] ) {
         
     return false;
     
-    });
-    
-    
-    /* Validate a user */
-    function setAdmin(id) 
-    {
-        var state = $("tr[data="+id+"] .btn-admin").attr("data-state");
-
-        if(confirm("Change Admin state?"))
-        {
-            var user = users[id];
-            
-            //toggle admin value
-            user.admin = !user.admin;
-            
-            $.ajax({
-                url: "/rest/admin/users/"+user.login,
-                type: "PUT",
-                dataType: "text",
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(user),
-                success:function(data){
-                    if(state == "true") {
-                        $("tr[data="+id+"] .btn-admin").removeClass("btn-warning");
-                        $("tr[data="+id+"] .btn-admin").attr("data-state","false");
-                    }
-                    else {
-                        $("tr[data="+id+"] .btn-admin").addClass("btn-warning");
-                        $("tr[data="+id+"] .btn-admin").attr("data-state","true");
-                    }
-                },
-                error:function(XMLHttpRequest, textStatus, errorThrows){
-                    alert("error:" + errorThrows)
-                }
-            })
-        };
-    };    
+    }); 
     
     
     </script>
