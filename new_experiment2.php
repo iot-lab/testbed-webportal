030f74e1d3c3180ec07144f5f970839c1328f031
evalentin <valentin@clarinet.u-strasbg.fr>
2012-05-24 15:28:14 +0200

    buidl exp json

--- new_experiment2.php
+++ new_experiment2.php
@@ -22,9 +22,11 @@ if(!$_SESSION['is_auth']) {
 	<select id="all_nodes" size="15" multiple></select>
         <select id="all_profils" size="15">
 		<option value="profil1">profil1</option>
+                <option value="profil2">profil2</option>
 	</select>
         <select id="all_firmwares" size="15">
 		<option value="firmware1">firmware1</option>
+                <option value="firmware2">firmware2</option>
 	</select>
 
 	<button id="btn_assoc" class="btn">Associate</button>
@@ -46,12 +48,15 @@ if(!$_SESSION['is_auth']) {
         
 
     <script type="text/javascript">
-        
-        $(document).ready(function(){
-		
-
+       
 	var exp_json_tmp = localStorage.getItem("exp_json");
 	var exp_json = JSON.parse(exp_json_tmp);
+
+	exp_json.profileassociations = [];
+        exp_json.firmwareassociations = [];
+
+        $(document).ready(function(){
+		
 
 	var selected_nodes = exp_json.nodes.split(",");
 	for(i=0;i<selected_nodes.length;i++)
@@ -71,13 +76,56 @@ if(!$_SESSION['is_auth']) {
 			return false;
 
 		$("#all_nodes option:selected").remove();
-
+	
+		var nodes_str = ""
 		for(i=0;i<nodes_set.length;i++)
 		{
+			nodes_str += nodes_set[i]+",";
 			$("#my_assoc").append("<li>" + nodes_set[i] + " > " + profil_set + " > " + firmware_set + "</li>");
 		}
 
 
+		var find = false;
+		//if profil already exist in the table
+		if(exp_json.profileassociations != null)
+		{
+			for(i=0;i<exp_json.profileassociations.length;i++) {
+				if(exp_json.profileassociations[i].profilename == profil_set)
+				{
+					exp_json.profileassociations[i].nodes += nodes_str;
+					find = true;
+				}
+			}
+		}		
+
+		if(!find) {
+			exp_json.profileassociations.push({"profilename":profil_set,"nodes":nodes_str});
+		}
+		
+
+                find = false;
+                //if firmware already exist in the table
+                if(exp_json.firmwareassociations != null)
+                {
+                        for(i=0;i<exp_json.firmwareassociations.length;i++) {
+                                if(exp_json.firmwareassociations[i].firmwarename == firmware_set)
+                                {
+                                        exp_json.firmwareassociations[i].nodes += nodes_str;
+                                        find = true;
+                                }
+                        }
+                }
+
+                if(!find) {
+                        exp_json.firmwareassociations.push({"firmwarename":firmware_set,"nodes":nodes_str});
+                }
+
+
+
+
+
+
+		console.log(JSON.stringify(exp_json)); 
 		return false;
 	});
 
