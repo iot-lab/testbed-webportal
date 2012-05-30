fd9684d783803fdbbd1bf163bfafb9a5bc081a9d
evalentin <valentin@clarinet.u-strasbg.fr>
2012-05-30 13:42:45 +0200

    indent code

--- new_experiment.php
+++ new_experiment.php
@@ -9,195 +9,157 @@ if(!$_SESSION['is_auth']) {
 
 <?php include("header.php") ?>
 
-    <div class="container">
-
-
-<h2>New experiment</h2>
-
-
-	<form class="well form-horizontal" id="form_new_exp">
-
-		<h3>1. Configure your experiment</h3>
-
-              <div class="control-group">
-                <label class="control-label" for="txt_name">Name:</label>
-                <div class="controls">
-                    <input id="txt_name" type="text" class="input-large" required="required">
+        <div class="container">
+            <h2>New experiment</h2>
+            <form class="well form-horizontal" id="form_new_exp">
+                <h3>1. Configure your experiment</h3>
+                <div class="control-group">
+                    <label class="control-label" for="txt_name">Name:</label>
+                    <div class="controls">
+                        <input id="txt_name" type="text" class="input-large" required="required">
+                    </div>
                 </div>
-              </div>
-
-              <div class="control-group">
-                <label class="control-label" for="txt_duration">Duration (minutes):</label>
-                <div class="controls">
-                    <input id="txt_duration" type="number" class="input-large" required="required">
+                <div class="control-group">
+                    <label class="control-label" for="txt_duration">Duration (minutes):</label>
+                    <div class="controls">
+                        <input id="txt_duration" type="number" class="input-large" required="required">
+                    </div>
                 </div>
-              </div>
-
-
-	<div class="control-group">
-            <label class="control-label">Execution</label>
-            <div class="controls">
-              <label class="radio">
-                <input type="radio" name="ExecutionType" id="optionsRadiosAsap" value="asap" checked="">
-                as soon as possible
-              </label>
-              <label class="radio">
-                <input type="radio" name="ExecutionType" id="optionsRadiosScheduled" value="scheduled">
-                scheduled
-              </label>
-            </div>
-          </div>
-
-
-	<h3>2. Select your nodes</h3>
-
-        <div class="control-group">
-            <label class="control-label">Resources</label>
-            <div class="controls">
-              <label class="radio">
-                <input type="radio" name="ResourcesType" id="optionsRadiosType" value="type" checked="">
-                by type
-              </label>
-
-	<!-- by type -->
-              <div class="" id="divResourcesType">
-                <label class="control-label" for="txt_fixed">Fixed:</label>
-                <div class="controls">
-                    <input id="txt_fixed" type="text" class="input-large">
+                <div class="control-group">
+                    <label class="control-label">Execution</label>
+                    <div class="controls">
+                        <label class="radio">
+                            <input type="radio" name="ExecutionType" id="optionsRadiosAsap" value="asap"
+                            checked="">as soon as possible</label>
+                        <label class="radio">
+                            <input type="radio" name="ExecutionType" id="optionsRadiosScheduled" value="scheduled">scheduled</label>
+                    </div>
                 </div>
-              </div>
+                <h3>2. Select your nodes</h3>
+                <div class="control-group">
+                    <label class="control-label">Resources</label>
+                    <div class="controls">
+                        <label class="radio">
+                            <input type="radio" name="ResourcesType" id="optionsRadiosType" value="type"
+                            checked="">by type</label>
+                        <!-- by type -->
+                        <div class="" id="divResourcesType">
+                            <label class="control-label" for="txt_fixed">Fixed:</label>
+                            <div class="controls">
+                                <input id="txt_fixed" type="text" class="input-large">
+                            </div>
+                        </div>
+                        <label class="radio">
+                            <input type="radio" name="ResourcesType" id="optionsRadiosMaps" value="physical">physical</label>
+                        <div class="" id="divResourcesMap">
+                            <p>
+                                <a href="#" id="str_maps">Strasbourg Maps</a>
+                                <input id="str_list" value="" />
+                            </p>
+                            <p>
+                                <a href="#" id="gre_maps">Grenoble Maps</a>
+                                <input id="gre_list" value="" />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <button id="btn_submit" class="btn btn-primary" type="submit">Next</button>
+            </form>
+            <?php include( 'footer.php') ?>
+        </div>
+        <script type="text/javascript">
+            $(document).ready(function () {
 
+                $("#divResourcesMap").hide();
+                $("input[name=ResourcesType]").change(function () {
+                    if ($(this).val() == "physical") {
+                        $("#divResourcesType").hide();
+                        $("#divResourcesMap").show();
+                    } else {
+                        $("#divResourcesType").show();
+                        $("#divResourcesMap").hide();
+                    }
 
-
-              <label class="radio">
-                <input type="radio" name="ResourcesType" id="optionsRadiosMaps" value="physical">
-                physical
-              </label>
-
-	        <div class="" id="divResourcesMap">
-                	
-                        <p><a href="#" id="str_maps">Strasbourg Maps</a> <input id="str_list" value=""/></p>
-                        <p><a href="#" id="gre_maps">Grenoble Maps</a> <input id="gre_list" value=""/></p>
-        	</div>
-
-          </div>
-	</div>
-
-
-	<button id="btn_submit" class="btn btn-primary" type="submit">Next</button>
-
-    </form>
-
-
-	<?php include('footer.php') ?>
-
-    </div>
-
-
-        
-
-    <script type="text/javascript">
-        
-        $(document).ready(function(){
-		
-
-		$("#divResourcesMap").hide();
-		$("input[name=ResourcesType]").change(function () {
-			if($(this).val() == "physical") {
-				$("#divResourcesType").hide();
-				$("#divResourcesMap").show();
-			}
-			else {
-				$("#divResourcesType").show();
-				$("#divResourcesMap").hide();
-			}
-
-		});
-
-		$("#str_maps").click(function(){
-			window.open('str_maps.php', '', 'resizable=no, location=no, width=500, height=500, menubar=no, status=no, scrollbars=no, menubar=no');
-		});
-
-                $("#gre_maps").click(function(){
-                        window.open('gre_maps.php', '', 'resizable=no, location=no, width=500, height=500, menubar=no, status=no, scrollbars=no, menubar=no');
                 });
 
-        });
- 
+                $("#str_maps").click(function () {
+                    window.open('str_maps.php', '', 'resizable=no, location=no, width=500, height=500, menubar=no, status=no, scrollbars=no, menubar=no');
+                });
 
-	$("#form_new_exp").bind("submit",function(){
+                $("#gre_maps").click(function () {
+                    window.open('gre_maps.php', '', 'resizable=no, location=no, width=500, height=500, menubar=no, status=no, scrollbars=no, menubar=no');
+                });
+
+            });
+
+
+            $("#form_new_exp").bind("submit", function () {
 
                 var exp_json = {
-                    "type":$("input[name=ResourcesType]:checked").val(),
-                    "name":$("#txt_name").val(),
-                    "duration":parseInt($("#txt_duration").val())
+                    "type": $("input[name=ResourcesType]:checked").val(),
+                    "name": $("#txt_name").val(),
+                    "duration": parseInt($("#txt_duration").val())
                 };
-	
-		//var my_nodes = new Array();
-		var my_nodes = "";
 
-		if($("#str_list").val() != "")
-		{
+                //var my_nodes = new Array();
+                var my_nodes = "";
 
-			var str_all = parseNodebox($("#str_list").val());
-			for(i=0;i<str_all.length;i++) {
-				my_nodes += "node"+str_all[i]+".devlille.senslab.info,";
-			}
-		}
+                if ($("#str_list").val() != "") {
 
-		if($("#gre_list").val() != "")
-                {
-                	var gre_all = parseNodebox($("#gre_list").val());
-                	for(i=0;i<gre_all.length;i++) {
-				my_nodes += "node"+gre_all[i]+".lyon.senslab.info,";
-                	}
-		}
+                    var str_all = parseNodebox($("#str_list").val());
+                    for (i = 0; i < str_all.length; i++) {
+                        my_nodes += "node" + str_all[i] + ".devlille.senslab.info,";
+                    }
+                }
+
+                if ($("#gre_list").val() != "") {
+                    var gre_all = parseNodebox($("#gre_list").val());
+                    for (i = 0; i < gre_all.length; i++) {
+                        my_nodes += "node" + gre_all[i] + ".lyon.senslab.info,";
+                    }
+                }
 
                 exp_json.nodes = [];
-		exp_json.nodes.push(my_nodes);
-		console.log(exp_json);
+                exp_json.nodes.push(my_nodes);
+                console.log(exp_json);
 
-		if(typeof localStorage!='undefined') {
-			localStorage.setItem("exp_json",JSON.stringify(exp_json));
-		} else {
-		}
+                if (typeof localStorage != 'undefined') {
+                    localStorage.setItem("exp_json", JSON.stringify(exp_json));
+                } else {}
 
-		window.location.href="new_experiment2.php";
+                window.location.href = "new_experiment2.php";
 
-		return false;
-	})
+                return false;
+            })
 
+            // expand a list of nodes containing dash intervals
+            // 1-3,5,9 -> 1,2,3,5,9
+            function expand(factExp) {
+                exp = [];
+                for (i = 0; i < factExp.length; i++) {
+                    dashExpression = factExp[i].split("-");
+                    if (dashExpression.length == 2) {
+                        for (j = parseInt(dashExpression[0]); j < (parseInt(dashExpression[1]) + 1); j++)
+                        exp.push(j);
+                    } else exp.push(parseInt(factExp[i]));
+                }
+                exp.sort(sortfunction);
+                for (var i = 1; i < exp.length; i++) {
+                    if (exp[i] == exp[i - 1]) {
+                        exp.splice(i--, 1);
+                    }
+                }
+                return exp;
+            }
 
+            function parseNodebox(input) {
+                return expand(input.split(","));
+            }
 
-// expand a list of nodes containing dash intervals
-// 1-3,5,9 -> 1,2,3,5,9
-function expand(factExp) {
-    exp = [];
-    for (i = 0; i < factExp.length; i++) {
-        dashExpression = factExp[i].split("-");
-        if (dashExpression.length == 2) {
-            for (j = parseInt(dashExpression[0]); j < (parseInt(dashExpression[1]) + 1); j++)
-                exp.push(j);
-        } else exp.push(parseInt(factExp[i]));
-    }
-    exp.sort(sortfunction);
-    for (var i = 1; i < exp.length; i++) { if (exp[i] == exp[i - 1]) { exp.splice(i--, 1); } }
-    return exp;
-}
-
-function parseNodebox(input) {
-    return expand(input.split(","));
-}
-
-function sortfunction(a, b) {
-    return (a - b) //causes an array to be sorted numerically and ascending
-}
-
-
-  
+            function sortfunction(a, b) {
+                return (a - b) //causes an array to be sorted numerically and ascending
+            }
+        </script>
+        </body>
         
-    </script>
-
-
-  </body>
-</html>
+        </html>
--- new_experiment2.php
+++ new_experiment2.php
@@ -9,221 +9,199 @@ if(!$_SESSION['is_auth']) {
 
 <?php include("header.php") ?>
 
-    <div class="container">
+        <div class="container">
+            <h2>New experiment</h2>
+            <form class="well form-horizontal" id="form_new_exp">
+                <h3>3. Configure your nodes</h3>
+                <p>
+                    <select id="all_nodes" size="15" multiple></select>
+                    <select id="all_profils" size="15">
+                        <option value="profil1">profil1</option>
+                        <option value="profil2">profil2</option>
+                    </select>
+                    <select id="all_firmwares" size="15">
+                    </select>
+                    <input type="file" id="files" name="files[]" multiple />
+                </p>
+                <p>
+                    <button id="btn_assoc" class="btn">Associate</button>
+                    <button id="btn_submit" class="btn btn-primary" type="submit">Submit</button>
+                </p>
+                <p>
+                    <table style="width:500px" class="table table-striped table-bordered table-condensed">
+                        <thead>
+                            <tr>
+                                <th>node</th>
+                                <th>profil</th>
+                                <th>firmware</th>
+                            </tr>
+                        </thead>
+                        <tbody id="my_assoc"></tbody>
+                    </table>
+                </p>
+            </form>
+            <?php include( 'footer.php') ?>
+        </div>
+        <script type="text/javascript">
+            var exp_json_tmp = localStorage.getItem("exp_json");
+            var exp_json = JSON.parse(exp_json_tmp);
 
+            var binary = [];
 
-<h2>New experiment</h2>
+            var withAssoc = false;
 
-	<form class="well form-horizontal" id="form_new_exp">
+            $(document).ready(function () {
 
-	<h3>3. Configure your nodes</h3>
+                var selected_nodes = exp_json.nodes[0].split(",");
+                for (i = 0; i < selected_nodes.length; i++) {
+                    if (selected_nodes[i] != "") $("#all_nodes").append(new Option(selected_nodes[i], selected_nodes[i], false, false));
+                }
 
-	<p>
-	<select id="all_nodes" size="15" multiple></select>
-        <select id="all_profils" size="15">
-		<option value="profil1">profil1</option>
-                <option value="profil2">profil2</option>
-	</select>
-        <select id="all_firmwares" size="15">
-		<!--<option value="firmware1">firmware1</option>
-                <option value="firmware2">firmware2</option>-->
-	</select>
-	<input type="file" id="files" name="files[]" multiple />
-	</p>
+                $("#form_new_exp").bind('submit', function () {
+                    console.log(JSON.stringify(exp_json));
 
-	<p>
-	
-	<button id="btn_assoc" class="btn">Associate</button>
-        <!-- <button id="btn_clear" class="btn btn-danger">Clear (TODO)</button> -->
-	<button id="btn_submit" class="btn btn-primary" type="submit">Submit</button>
-	</p>
+                    var url = "/rest/experiments?body";
+                    var content_type = "application/json; charset=utf-8";
+                    var data = JSON.stringify(exp_json);
+                    var datab = JSON.stringify(exp_json);
 
-	<p>
-		<table style="width:500px"class="table table-striped table-bordered table-condensed">
-		<thead><tr><th>node</th><th>profil</th><th>firmware</th></tr></thead>
-		<tbody id="my_assoc"></tbody>
-		</table>
-	</p>
+                    var boundary = "----------------------------a9c9cb8394e1";
 
-    </form>
+                    if (withAssoc) {
+                        url = "/rest/experiments";
+                        content_type = "multipart/form-data; boundary=" + boundary;
+                        data = "";
 
-	<?php include('footer.php') ?>
-
-    </div>
-
-
-    <script type="text/javascript">
-       
-	var exp_json_tmp = localStorage.getItem("exp_json");
-	var exp_json = JSON.parse(exp_json_tmp);
-
-	var binary = [];
-
-
-        var withAssoc = false;
-
-        $(document).ready(function(){
-		
-
-	var selected_nodes = exp_json.nodes[0].split(",");
-	for(i=0;i<selected_nodes.length;i++)
-	{
-		if(selected_nodes[i] != "")
-			$("#all_nodes").append(new Option(selected_nodes[i],selected_nodes[i] , false, false));
-	}
-
-
-        $("#form_new_exp").bind('submit',function(){
-	       console.log(JSON.stringify(exp_json));
-
-	var url = "/rest/experiments?body";
-        var content_type = "application/json; charset=utf-8";
-	var data = JSON.stringify(exp_json);
-        var datab = JSON.stringify(exp_json);
-
-	var boundary = "----------------------------a9c9cb8394e1";
-
-	if(withAssoc) {
-		url = "/rest/experiments";
-		content_type = "multipart/form-data; boundary="+boundary;
-		data = "";
-	
-		for(i=0;i<binary.length;i++)
-		{
-			data += boundary + '\r\n';
-			data += 'Content-Disposition: form-data; name="'+binary[i].name+'" filename="'+binary[i].name+'"\r\n';
-                	data += 'Content-Type: text/plain\r\n\r\n';
-			data += binary[i].bin + '\r\n';
-		}
-
-		//add json
-                data += boundary + '\r\n';
-                data += 'Content-Disposition: form-data; name="test.json" filename="test.json"\r\n';
-                data += 'Content-Type: text/json\r\n\r\n';
-                data += datab + '\r\n\r\n';		
-
-
-		data += boundary + '--\r\n';
-		
-		//data = new FormData();
-		//data.append(binary[0].name,binary[0].bin);
-	}
-
-        $.ajax({
-            url: url,
-            type: "POST",
-            dataType: "text",
-            contentType: content_type,
-            data: data,
-            processData:false,
-            success:function(data){
-                 alert("ok: " + data );
-            },
-            error:function(XMLHttpRequest, textStatus, errorThrows){
-		 alert("error: " + errorThrows );
-            }
-        });
-
-	       return false;
-        });
-
-	$("#btn_assoc").click(function(){
-
-                if(!withAssoc) {
-			exp_json.profileassociations = [];
-        		exp_json.firmwareassociations = [];
-			withAssoc = true;
-		}
-
-		var nodes_set = $("#all_nodes").val();
-		var profil_set = $("#all_profils").val();
-                var firmware_set = $("#all_firmwares").val();
-
-		if(nodes_set == null || profil_set == null || firmware_set == null)
-			return false;
-
-		$("#all_nodes option:selected").remove();
-	
-		var nodes_str = ""
-		for(i=0;i<nodes_set.length;i++)
-		{
-			nodes_str += nodes_set[i]+",";
-			$("#my_assoc").append("<tr><td>" + nodes_set[i] + "</td><td>" + profil_set + "</td><td>" + firmware_set + "</td></tr>");
-		}
-
-
-
-		var find = false;
-		//if profil already exist in the table
-		if(exp_json.profileassociations != null)
-		{
-			for(i=0;i<exp_json.profileassociations.length;i++) {
-				if(exp_json.profileassociations[i].profilename == profil_set)
-				{
-					exp_json.profileassociations[i].nodes += nodes_str;
-					find = true;
-				}
-			}
-		}		
-
-		if(!find) {
-			exp_json.profileassociations.push({"profilename":profil_set,"nodes":nodes_str});
-		}
-		
-
-                find = false;
-                //if firmware already exist in the table
-                if(exp_json.firmwareassociations != null)
-                {
-                        for(i=0;i<exp_json.firmwareassociations.length;i++) {
-                                if(exp_json.firmwareassociations[i].firmwarename == firmware_set)
-                                {
-                                        exp_json.firmwareassociations[i].nodes += nodes_str;
-                                        find = true;
-                                }
+                        for (i = 0; i < binary.length; i++) {
+                            data += boundary + '\r\n';
+                            data += 'Content-Disposition: form-data; name="' + binary[i].name + '" filename="' + binary[i].name + '"\r\n';
+                            data += 'Content-Type: text/plain\r\n\r\n';
+                            data += binary[i].bin + '\r\n';
                         }
+
+                        //add json
+                        data += boundary + '\r\n';
+                        data += 'Content-Disposition: form-data; name="test.json" filename="test.json"\r\n';
+                        data += 'Content-Type: text/json\r\n\r\n';
+                        data += datab + '\r\n\r\n';
+
+                        data += boundary + '--\r\n';
+
+                        //data = new FormData();
+                        //data.append(binary[0].name,binary[0].bin);
+                    }
+
+                    $.ajax({
+                        url: url,
+                        type: "POST",
+                        dataType: "text",
+                        contentType: content_type,
+                        data: data,
+                        processData: false,
+                        success: function (data) {
+                            alert("ok: " + data);
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrows) {
+                            alert("error: " + errorThrows);
+                        }
+                    });
+
+                    return false;
+                });
+
+                $("#btn_assoc").click(function () {
+
+                    if (!withAssoc) {
+                        exp_json.profileassociations = [];
+                        exp_json.firmwareassociations = [];
+                        withAssoc = true;
+                    }
+
+                    var nodes_set = $("#all_nodes").val();
+                    var profil_set = $("#all_profils").val();
+                    var firmware_set = $("#all_firmwares").val();
+
+                    if (nodes_set == null || profil_set == null || firmware_set == null) return false;
+
+                    $("#all_nodes option:selected").remove();
+
+                    var nodes_str = ""
+                    for (i = 0; i < nodes_set.length; i++) {
+                        nodes_str += nodes_set[i] + ",";
+                        $("#my_assoc").append("<tr><td>" + nodes_set[i] + "</td><td>" + profil_set + "</td><td>" + firmware_set + "</td></tr>");
+                    }
+
+                    var find = false;
+                    //if profil already exist in the table
+                    if (exp_json.profileassociations != null) {
+                        for (i = 0; i < exp_json.profileassociations.length; i++) {
+                            if (exp_json.profileassociations[i].profilename == profil_set) {
+                                exp_json.profileassociations[i].nodes += nodes_str;
+                                find = true;
+                            }
+                        }
+                    }
+
+                    if (!find) {
+                        exp_json.profileassociations.push({
+                            "profilename": profil_set,
+                            "nodes": nodes_str
+                        });
+                    }
+
+                    find = false;
+                    //if firmware already exist in the table
+                    if (exp_json.firmwareassociations != null) {
+                        for (i = 0; i < exp_json.firmwareassociations.length; i++) {
+                            if (exp_json.firmwareassociations[i].firmwarename == firmware_set) {
+                                exp_json.firmwareassociations[i].nodes += nodes_str;
+                                find = true;
+                            }
+                        }
+                    }
+
+                    if (!find) {
+                        exp_json.firmwareassociations.push({
+                            "firmwarename": firmware_set,
+                            "nodes": nodes_str
+                        });
+                    }
+
+
+                    console.log(JSON.stringify(exp_json));
+                    return false;
+                });
+
+            });
+
+
+            function handleFileSelect(evt) {
+                var files = evt.target.files; // FileList object
+
+                // Loop through the FileList and render image files as thumbnails.
+                for (var i = 0, f; f = files[i]; i++) {
+
+                    var reader = new FileReader();
+
+                    // Closure to capture the file information.
+                    reader.onload = (function (theFile) {
+                        return function (e) {
+
+                            binary.push({
+                                "name": theFile.name,
+                                "bin": e.target.result
+                            });
+
+                            $("#all_firmwares").append(new Option(theFile.name, theFile.name, false, false));
+                        };
+                    })(f);
+
+                    reader.readAsText(f);
                 }
+            }
 
-                if(!find) {
-                        exp_json.firmwareassociations.push({"firmwarename":firmware_set,"nodes":nodes_str});
-                }
-
-
-		console.log(JSON.stringify(exp_json)); 
-		return false;
-	});
-
-	});
+            document.getElementById('files').addEventListener('change', handleFileSelect, false);
+        </script>
+        </body>
         
-
-
-function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-      var reader = new FileReader();
-
-      // Closure to capture the file information.
-      reader.onload = (function(theFile) {
-        return function(e) {
-
-	  binary.push({"name":theFile.name,"bin":e.target.result});
-
-          $("#all_firmwares").append(new Option(theFile.name,theFile.name,false,false));
-        };
-      })(f);
-
-      reader.readAsText(f);
-    }
-  }
-
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
-
-
-    </script>
-
-
-  </body>
-</html>
+        </html>
