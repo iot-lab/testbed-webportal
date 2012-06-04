fe049bad88cdf9ed4e8f50887d1931b2c468e395
Loic Schmidt <loic.schmidt@inria.fr>
2012-06-04 12:12:41 +0200

    Pagination in experiments list 5

--- list_experiments.php
+++ list_experiments.php
@@ -8,7 +8,43 @@ if(!$_SESSION['is_auth'] || ($_SESSION['login'] == "" || $_SESSION['password'] =
     exit();
 }
 
-$url = "https://localhost/rest/experiments?limit=".$_GET['iDisplayLength']."&offset=".$_GET['iDisplayStart'];
+
+/* Get total */
+$url = "https://localhost/rest/experiments?total";
+
+$handle = curl_init();
+curl_setopt($handle, CURLOPT_URL, $url);
+curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($handle, CURLOPT_HEADER, false);
+curl_setopt($handle, CURLOPT_VERBOSE, true);
+
+curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($handle, CURLOPT_USERPWD, $_SESSION['login'] . ":" . $_SESSION['password']);
+curl_setopt($handle, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+$response = curl_exec($handle);
+$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+curl_close($handle);
+
+if($code == 200) {
+	$response2=json_decode($response);
+	$total=$response2->{'upcoming'}+$response2->{'terminated'}+$response2->{'running'};
+}
+
+$offset = $total - $_GET['iDisplayLength'] - $_GET['iDisplayStart'];
+if($offset<0) $offset = 0;
+$limit = $_GET['iDisplayLength'];
+if($total<$_GET['iDisplayLength']+$_GET['iDisplayStart']) $limit = $total % $_GET['iDisplayLength'];
+
+
+/*echo $total." / ".$_GET['iDisplayLength']." / ".$_GET['iDisplayStart']."<br/>";
+echo $total." / ".$limit." / ".$offset."<br/>";
+echo "<hr/>";*/
+
+$url = "https://localhost/rest/experiments?state=Terminated,Error,Running,Finnishing,Resuming,toError,Waiting,Launching,Hold,toLaunch,toAckReservation,Suspended&limit=".$limit."&offset=".$offset;
 $handle = curl_init();
 curl_setopt($handle, CURLOPT_URL, $url);
 curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
@@ -39,7 +75,7 @@ if($code != 200) {
 	$nb_items=count($response2->{'items'});
 	
 	for ($i=0;$i<$nb_items;$i++) {
-		$item = $response2->{'items'}[$i];
+		$item = $response2->{'items'}[$nb_items-$i-1];
 		$buttons='<a href=\"#\" class=\"btn btn-valid\" data=\"'.$item->{'id'}.'\" onClick=\"detailsExp('.$item->{'id'}.')\">Details</a>';
 		/*if(strcmp($item->{'state'},"Running")==0
 			|| strcmp($item->{'state'},"Finishing")==0
