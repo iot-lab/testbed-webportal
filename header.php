715cd2f8dbc47e82a08accd7b2581b5657e5811d
evalentin <valentin@clarinet.u-strasbg.fr>
2012-06-13 20:34:56 +0200

    https error when fetched http file

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="js/html5.js"></script>
 <![endif]-->
 
   <script src="js/jquery.js"></script>
