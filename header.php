aee9989793587a44d5b7062196e7f8256e231bf2
Loic Schmidt <loic.schmidt@inria.fr>
2012-06-15 09:56:55 +0200

    Webportal: css colors links and inputs

--- header.php
+++ header.php
@@ -47,7 +47,16 @@
     a {
         color: #AAD400;
     }
-
+    
+    a:hover {
+        color: #AAD400;
+        text-decoration:underline;
+    }
+input:focus, textarea:focus {
+    border-color: rgba(170, 212, 0, 0.8);
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px rgba(170, 212, 0, 0.6);
+    outline: 0 none;
+}
 
 </style>
 
