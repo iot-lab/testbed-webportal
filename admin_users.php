3044391a0f1fd86217db6ad17c2c761f56871d49
Loic Schmidt <loic.schmidt@inria.fr>
2012-07-31 15:52:52 +0200

    Webportal: user experiments link in admin user list

--- admin_users.php
+++ admin_users.php
@@ -265,7 +265,7 @@ include("header.php") ?>
                     '<td>'+ formatCreateTimeStamp(val.createTimeStamp) +'</td>'+
                     '<td><a href="#" class="btn btn-valid '+btnValidClass+'" data="'+i+'" data-state="'+val.validate+'" onClick="validateUser('+i+')">'+btnValidValue+'</a></td>' +
                     '<td><a href="#" class="btn btn-admin '+btnAdminClass+'" data="'+i+'" data-state="'+val.admin+'" onClick="setAdmin('+i+')">'+btnAdminValue+'</a></td>' +
-                    '<td><a href="admin_manageexps.php?user='+val.login+'" class="btn btn-view">Exp</a> ' +
+                    '<td><a href="admin_exps.php?user='+val.login+'" class="btn btn-view">Exp</a> ' +
                         '<a href="#" class="btn btn-edit" data-toggle="modal" data="'+i+'">Edit</a> ' +
                         '<a href="#" class="btn btn-del btn-danger" data="'+i+'" onClick="deleteUser('+i+')">Delete</a></td>'
                     +'</tr>');
