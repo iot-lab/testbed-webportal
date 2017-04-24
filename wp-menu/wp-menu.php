90405051665710020a1dbee54742f8a103131fb1
Benoit Formet <benoit.formet@inria.fr>
2017-04-24 17:54:37 +0200

    update wordpress menu

--- wp-menu/wp-menu.php
+++ wp-menu/wp-menu.php
@@ -1,34 +1,38 @@
-<ul id="menu-topmainmenu-1" class="nav navbar-nav"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-140 dropdown"><a title="Platform" data-hover="dropdown" href="https://devgrenoble.senslab.info/www/what-is-iot-lab/">Platform <span class="caret"></span></a>
+<ul id="menu-topmainmenu-1" class="nav navbar-nav"><li class="menu-item menu-item-type-post_type menu-item-object-page current_page_parent menu-item-2066"><a title="News" href="https://devwww.iot-lab.info/news/">News</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-140 dropdown"><a title="Platform" data-toggle="dropdown" data-hover="dropdown" href="https://devwww.iot-lab.info/what-is-iot-lab/">Platform <span class="caret"></span></a>
 <ul role="menu" class=" dropdown-menu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-466"><a title="What is IoT-LAB?" href="https://devgrenoble.senslab.info/www/what-is-iot-lab/">What is IoT-LAB?</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-147"><a title="Deployment" href="https://devgrenoble.senslab.info/www/deployment/">Deployment</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146"><a title="Hardware" href="https://devgrenoble.senslab.info/www/hardware/">Hardware</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-441"><a title="FIT" href="https://devgrenoble.senslab.info/www/fit/">FIT</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-466"><a title="What is IoT-LAB?" href="https://devwww.iot-lab.info/what-is-iot-lab/">What is IoT-LAB?</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-147"><a title="Deployment" href="https://devwww.iot-lab.info/deployment/">Deployment</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146"><a title="Hardware" href="https://devwww.iot-lab.info/hardware/">Hardware</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4608"><a title="Robots" href="https://devwww.iot-lab.info/robots/">Robots</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2400"><a title="About us" href="https://devwww.iot-lab.info/about-us/">About us</a></li>
 </ul>
 </li>
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-163 dropdown"><a title="Dev Center" data-hover="dropdown" href="https://devgrenoble.senslab.info/www/dev-center/">Dev Center <span class="caret"></span></a>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1467 dropdown"><a title="Dev Center" data-toggle="dropdown" data-hover="dropdown" href="https://devwww.iot-lab.info/dev-center/">Dev Center <span class="caret"></span></a>
 <ul role="menu" class=" dropdown-menu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-168"><a title="Tutorials" href="https://devgrenoble.senslab.info/www/dev-center/tutorials/">Tutorials</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-167"><a title="Drivers" href="https://devgrenoble.senslab.info/www/dev-center/drivers/">Drivers</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-166"><a title="Libraries" href="https://devgrenoble.senslab.info/www/dev-center/libraries/">Libraries</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-165"><a title="Operating Systems" href="https://devgrenoble.senslab.info/www/dev-center/operating-systems/">Operating Systems</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-164"><a title="REST API docs" href="https://devgrenoble.senslab.info/www/dev-center/rest-api-docs/">REST API docs</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1957"><a title="Software Development" href="https://devwww.iot-lab.info/dev-center/">Software Development</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-168"><a title="Tutorials" href="https://devwww.iot-lab.info/tutorials/">Tutorials</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1552"><a title="Operating Systems" href="https://devwww.iot-lab.info/operating-systems/">Operating Systems</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1396"><a title="Libraries" href="https://devwww.iot-lab.info/libraries/">Libraries</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1394"><a title="Drivers" href="https://devwww.iot-lab.info/drivers/">Drivers</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1489"><a title="Experimentation Tools" href="https://devwww.iot-lab.info/tools/">Experimentation Tools</a></li>
 </ul>
 </li>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-26"><a title="Community">Community</a></li>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-73 dropdown"><a title="Pages tests" data-hover="dropdown">Pages tests <span class="caret"></span></a>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3209 dropdown"><a title="Community" data-toggle="dropdown" data-hover="dropdown" href="https://devwww.iot-lab.info/community/">Community <span class="caret"></span></a>
 <ul role="menu" class=" dropdown-menu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47"><a title="About us" href="https://devgrenoble.senslab.info/www/test-new-page/">About us</a></li>
-	<li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-77"><a title="News" href="https://devgrenoble.senslab.info/www/category/news/">News</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-229"><a title="GeSHi Source Colorer" href="https://devgrenoble.senslab.info/www/geshi-source-colorer/">GeSHi Source Colorer</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3244"><a title="Share your experience" href="https://devwww.iot-lab.info/community/">Share your experience</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3210"><a title="Education" href="https://devwww.iot-lab.info/education/">Education</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2133"><a title="Publications" href="https://devwww.iot-lab.info/publications/">Publications</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2405"><a title="Charter" href="https://devwww.iot-lab.info/charter/">Charter</a></li>
 </ul>
 </li>
-<?php if(isset($_SESSION['is_auth']) && $_SESSION['is_auth'] && isset($_SESSION['is_admin']) && $_SESSION['is_admin']) { ?>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-241 dropdown"><a title="Admin" data-hover="dropdown">Admin <span class="caret"></span></a>
+<li class="get_started menu-item menu-item-type-post_type menu-item-object-page menu-item-1470"><a title="Get Started" href="https://devwww.iot-lab.info/get_started/">Get Started</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-241 dropdown"><a title="Admin" data-toggle="dropdown" data-hover="dropdown">Admin <span class="caret"></span></a>
 <ul role="menu" class=" dropdown-menu">
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-556"><a title="Write an API page" href="https://devgrenoble.senslab.info/www/write-an-api-page/">Write an API page</a></li>
-	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-581"><a title="WP mods for IoT-LAB" href="https://devgrenoble.senslab.info/www/wp-mods-for-iot-lab/">WP mods for IoT-LAB</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-581"><a title="WP mods for IoT-LAB" href="https://devwww.iot-lab.info/admin/wp-mods-for-iot-lab/">WP mods for IoT-LAB</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-640"><a title="Maintaining our WordPress" href="https://devwww.iot-lab.info/admin/maintaining-our-wordpress/">Maintaining our WordPress</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1987"><a title="Style guide" href="https://devwww.iot-lab.info/admin/style-guide/">Style guide</a></li>
+	<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2058"><a title="Write a news" href="https://devwww.iot-lab.info/admin/write-a-news/">Write a news</a></li>
 </ul>
 </li>
-<?php } ?>
 </ul>
