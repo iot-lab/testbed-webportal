b6c6f4db71b2951a7de23313db2b1054d159a482
Benoit Formet <benoit.formet@inria.fr>
2017-04-27 17:42:07 +0200

    add fabric deployment script

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import local, run, put, lcd
+
+def test():
+    """ Run Web portal tests. """
+    local('npm test')
+    
+
+def build():
+    """ Build Web portal (to ./dist). """
+    local('npm run build')
+    
+def deploy(dist="./dist"):
+    """ Deploy Web portal. """
+    with lcd(dist):
+        run('rm -Rf /tmp/devportal/')
+        run('mkdir /tmp/devportal')
+        put("*", "/tmp/devportal/")
+    run('ssh srvwww \"rm -Rf /var/www/devportal/*\"')
+    run('scp -r /tmp/devportal/* srvwww:/var/www/devportal/')
+    # run('ssh srvwww \"chown root:www-data /var/www/devportal/wp-menu/\"')
+    # run('ssh srvwww \"chmod g+w /var/www/devportal/wp-menu/\"')
+    # run('ssh srvwww \"rm /var/www/devportal/wp-menu/wp-menu.php\"')
+    run('rm -Rf /tmp/devportal/')
