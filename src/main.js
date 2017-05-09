3ffc229b5f6baa2289be38d821a710acc57e8e61
Benoit Formet <benoit.formet@inria.fr>
2017-05-09 14:15:08 +0200

    put routes apart

--- src/main.js
+++ src/main.js
@@ -1,25 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
-
-Vue.use(VueResource)
+import {auth} from './auth'
 
 Vue.config.productionTip = false
 
-let loggedIn = false
-
-function doLogin () {
-  loggedIn = true
-}
-function doLogout () {
-  loggedIn = false
-}
-
 router.beforeEach((to, from, next) => {
-  if (!loggedIn && to.meta.requiresAuth) {
+  if (!auth.loggedIn && to.meta.requiresAuth) {
     next('login')
   } else {
     next()
@@ -32,5 +21,3 @@ new Vue({
   template: '<App/>',
   components: { App },
 }).$mount('#app')
-
-export {doLogin, doLogout}
--- src/router.js
+++ src/router.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
+import ResetPassword from '@/components/ResetPassword'
 import Signup from '@/components/Signup'
 import UserAccount from '@/components/UserAccount'
-import Monitoring from '@/components/Monitoring'
+import Monitor from '@/components/Monitor'
 import Experiment from '@/components/Experiment'
 import Dashboard from '@/components/Dashboard'
 
@@ -15,9 +16,10 @@ export default new Router({
     { path: '/', redirect: '/dashboard' },
     { path: '/dashboard', name: 'dashboard', component: Dashboard, meta: { requiresAuth: true } },
     { path: '/experiment', name: 'experiment', component: Experiment, meta: { requiresAuth: true } },
-    { path: '/monitoring', name: 'monitoring', component: Monitoring, meta: { requiresAuth: true } },
+    { path: '/monitor', name: 'monitor', component: Monitor, meta: { requiresAuth: true } },
     { path: '/UserAccount', name: 'UserAccount', component: UserAccount, meta: { requiresAuth: true } },
     { path: '/signup', name: 'signup', component: Signup },
+    { path: '/reset', name: 'reset', component: ResetPassword },
     { path: '/login', name: 'login', component: Login },
     { path: '/hello', name: 'hello', component: Hello },
   ],
