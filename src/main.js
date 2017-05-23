11121f5c11b847c0cfc18471604548ada6cd7f85
Benoit Formet <benoit.formet@inria.fr>
2017-05-23 19:36:50 +0200

    router cleaning

--- src/main.js
+++ src/main.js
@@ -3,17 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {auth} from './auth'
 
 Vue.config.productionTip = false
-
-router.beforeEach((to, from, next) => {
-  if (!auth.loggedIn && to.meta.requiresAuth) {
-    next('login')
-  } else {
-    next()
-  }
-})
 
 /* eslint-disable no-new */
 new Vue({
--- src/router.js
+++ src/router.js
@@ -8,19 +8,31 @@ import UserAccount from '@/components/UserAccount'
 import Monitor from '@/components/Monitor'
 import Experiment from '@/components/Experiment'
 import Dashboard from '@/components/Dashboard'
+import {auth} from './auth'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     { path: '/', redirect: '/dashboard' },
     { path: '/dashboard', name: 'dashboard', component: Dashboard, meta: { requiresAuth: true } },
     { path: '/experiment', name: 'experiment', component: Experiment, meta: { requiresAuth: true } },
     { path: '/monitor', name: 'monitor', component: Monitor, meta: { requiresAuth: true } },
-    { path: '/UserAccount', name: 'UserAccount', component: UserAccount, meta: { requiresAuth: true } },
+    { path: '/account', name: 'UserAccount', component: UserAccount, meta: { requiresAuth: true } },
     { path: '/signup', name: 'signup', component: Signup },
     { path: '/reset', name: 'reset', component: ResetPassword },
     { path: '/login', name: 'login', component: Login },
     { path: '/hello', name: 'hello', component: Hello },
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page when appropriate
+  if (to.meta.requiresAuth && !auth.loggedIn) {
+    next('login')
+  } else {
+    next()
+  }
+})
+
+export default router
