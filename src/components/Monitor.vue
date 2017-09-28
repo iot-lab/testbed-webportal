1df28b7044bd1bfab4dce2e59bd8612560696383
Benoit Formet <benoit.formet@inria.fr>
2017-09-28 10:36:18 +0200

    navbar rename Monitor -> Monitoring

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,15 +1,29 @@
 <template>
-<div class="container mt-3">
-        
-    <h2>Monitor</h2>
+<div class="container my-3">
+  <h2 class="mb-4">Monitoring profiles</h2>
+
 
 </div> <!-- container -->
 
 </template>
 
 <script>
+// import {iotlab} from '@/rest'
+
 export default {
-  name: 'Monitor',
+  name: 'Monitoring',
+
+  data () {
+    return {
+    }
+  },
+
+  created () {
+    // iotlab.getUsers().then(data => { this.users = data })
+  },
+
+  methods: {
+  },
 }
 </script>
 
