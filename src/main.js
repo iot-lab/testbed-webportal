4ef0827a372644aec6499706d4573cb2e7b03d7f
Benoit Formet <benoit.formet@inria.fr>
2017-10-23 18:14:33 +0200

    signup: use form validation lib & show nice red borders for invalid fields (vee-validate)

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
 import Notifications from 'vue-notification'
 import velocity from 'velocity-animate'
 import App from './App'
 import router from './router'
+import {iotlab} from './rest'
 import './filters'
 
+Vue.use(VeeValidate)
 Vue.use(Notifications, {velocity})
 
 Vue.config.productionTip = false
+
+window.iotlab = iotlab
 
 /* eslint-disable no-new */
 new Vue({
