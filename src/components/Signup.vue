8377d1500b1c70d0866ad2a291d1e4e0bd0b2196
Benoit Formet <benoit.formet@inria.fr>
2017-10-23 18:14:33 +0200

    signup: use form validation lib & show nice red borders for invalid fields (vee-validate)

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -9,7 +9,7 @@
           <h4>Check your inbox and follow link to validate your account.</h4>
         </div>
       </div>
-      <form v-else class="mb-5" @submit.prevent="signup" novalidate>
+      <form v-else class="mb-5" @submit.prevent="signup">
         <div class="text-right text-italic text-muted small">
           * All fields are mandatory
         </div>
@@ -17,32 +17,55 @@
           <div class="col-md">
             <div class="form-group">
               <label class="form-control-label">First name</label>
-              <input placeholder="First name" v-model="user.firstName" class="form-control" type="text" required>
+              <input placeholder="First name" v-model="user.firstName" name="firstname" class="form-control" type="text" v-validate="'required'" :class="{'is-invalid': errors.has('firstname') }">
+              <div class="invalid-feedback" v-show="errors.has('firstname')">
+                {{ errors.first('firstname') }}
+              </div>
             </div>
           </div>
           <div class="col-md">                    
             <div class="form-group">
               <label class="form-control-label">Last name</label>
-              <input placeholder="Last name" v-model="user.lastName" class="form-control" type="text" required>
+              <input placeholder="Last name" v-model="user.lastName" name="lastname" class="form-control" type="text" v-validate="'required'" :class="{'is-invalid': errors.has('lastname') }">
+              <div class="invalid-feedback" v-show="errors.has('lastname')">
+                {{ errors.first('lastname') }}
+              </div>
             </div>
           </div>
         </div>
             <div class="form-group">
               <label class="form-control-label">Email</label>
-              <input v-model="user.email" class="form-control" type="email" required placeholder="Academic or professional email">
-              <span class="text-danger" style="font-size: 0.9rem">Please fill with an <b>academic</b> or <b>professional</b> email in order to validate your account <b>(no gmail, hotmail, &#x2026;)</b></span>
+              <input v-model="user.email" name="email" class="form-control" type="email" v-validate="'required|email'" placeholder="Academic or professional email" :class="{'is-invalid': errors.has('email') }">
+              <div class="invalid-feedback" v-show="errors.has('email')">
+                {{ errors.first('email') }}
+              </div>
             </div>
         <div class="row">
           <div class="col-md">
             <div class="form-group">
               <label class="form-control-label">User category</label>
-              <multiselect v-model="user.category" :options="Object.keys(categories)" placeholder="Category" :searchable="false" required :custom-label="userLabel" :show-labels="false"></multiselect>
-            </div>                    
+              <multiselect v-model="user.category"
+                placeholder="Category"
+                :options="Object.keys(categories)"
+                :allow-empty="false"
+                :searchable="false"
+                :show-labels="false"
+                :custom-label="userLabel"
+                :class="{'mymultiselect': true, 'invalid': dirty.category && !user.category}"
+                @close="dirty.category = true">
+              </multiselect>
+              <div class="invalid-feedback" :style="{'display': (dirty.category && !user.category) ? 'block': 'none'}">
+                The user category field is required.
+              </div>
+            </div>
           </div>
           <div class="col-md">
             <div class="form-group">
               <label class="form-control-label">Organization</label>
-              <input placeholder="Organization" v-model="user.organization" class="form-control" type="text" required>
+              <input placeholder="Organization" v-model="user.organization" name="organization" class="form-control" type="text" v-validate="'required'" data-vv-rules="required" :class="{'is-invalid': errors.has('organization') }">
+              <div class="invalid-feedback" v-show="errors.has('organization')">
+                {{ errors.first('organization') }}
+              </div>
             </div>
           </div>
         </div>
@@ -50,21 +73,44 @@
           <div class="col-md">
             <div class="form-group">
               <label class="form-control-label">City</label>
-              <input placeholder="City" v-model="user.city" class="form-control" type="text" required>
+              <input placeholder="City" v-model="user.city" name="city" class="form-control" type="text" v-validate="'required'" :class="{'is-invalid': errors.has('city') }">
+              <div class="invalid-feedback" v-show="errors.has('city')">
+                {{ errors.first('city') }}
+              </div>
             </div>
           </div>
           <div class="col-md">
             <div class="form-group">
               <label class="form-control-label">Country</label>
-              <multiselect :options="countries" v-model="user.country" required placeholder="Country"></multiselect>
+              <multiselect v-model="user.country"
+                placeholder="Country"
+                :options="countries"
+                :class="{'mymultiselect': true, 'invalid': dirty.country && !user.country}"
+                @close="dirty.country = true">
+              </multiselect>
+              <div class="invalid-feedback" :style="{'display': (dirty.country && !user.country) ? 'block': 'none'}">
+                The country field is required.
+              </div>
             </div>
           </div>
         </div>
         <div class="form-group">
           <label class="form-control-label">Motivations</label>
-          <textarea v-model="user.motivations" class="form-control" rows="5" placeholder="Tell us about you" required></textarea>
+          <textarea v-model="user.motivations"
+            name="motivations"
+            class="form-control"
+            rows="5"
+            placeholder="Tell us about you"
+            v-validate="'required'"
+            @focus="toggleMotivations"
+            @blur="toggleMotivations"
+            :class="{'is-invalid': errors.has('motivations') }">
+          </textarea>
+          <div class="invalid-feedback" v-show="errors.has('motivations')">
+            {{ errors.first('motivations') }}
+          </div>
         </div>
-        <div class="form-group">
+        <div class="form-group" id="motivations" style="display: none">
           <div class="alert alert-info form-control">
             <i class="fa fa-pencil"></i> <b>Tell us about your motivations</b>
             <ul style="margin: 0">
@@ -76,16 +122,22 @@
           </div>
         </div>
         <div class="form-group">
-          <vue-recaptcha ref="recaptcha" @verify="onCaptchaVerify" @expired="onCaptchaExpired" :sitekey="reCaptchaSitekey"></vue-recaptcha>
+          <vue-recaptcha ref="recaptcha" @verify="onCaptchaVerify" @expired="onCaptchaExpired"
+            :sitekey="reCaptchaSitekey"
+            :class="(!captchaVerified && dirty.captcha) ? 'border-danger form-control p-2' : ''">
+          </vue-recaptcha>
         </div>
         <div class="form-check">
           <label class="custom-control custom-checkbox">
-            <input v-model="charter" type="checkbox" class="custom-control-input">
+            <input v-model="charter" name="charter" type="checkbox" class="custom-control-input" v-validate="'required'" :class="{'is-invalid': errors.has('charter') }">
             <span class="custom-control-indicator"></span>
             <span class="custom-control-description">
-              I read and I accept <a href="/charter/" target="_blank">IoT-LAB Terms of Service</a>.
+              I read and I accept <a href="/charter/" :class="{'text-danger': errors.has('charter') }" target="_blank">IoT-LAB Terms of Service</a>.
             </span>
           </label>
+          <div class="invalid-feedback mb-3" :class="{'d-block': errors.has('charter') }" v-show="errors.has('charter')">
+            You must check the box and accept the terms of service.
+          </div>
         </div>
         <div class="form-group">
           <button class="btn btn-lg btn-primary" type="submit">Sign up</button>
@@ -105,6 +157,7 @@ import VueRecaptcha from 'vue-recaptcha'
 import {iotlab} from '@/rest'
 import countries from '@/assets/js/countries'
 import UserCategories from '@/assets/js/categories'
+import $ from 'jquery'
 
 export default {
   name: 'signup',
@@ -114,46 +167,57 @@ export default {
     return {
       reCaptchaSitekey: '6Ld8cR4UAAAAAC-zBLP9m2bC35xyyYwTbvkBcx4q',
       charter: false,
-      verified: false,
+      captchaVerified: false,
       user: {},
       countries: countries,
       categories: UserCategories,
       category: undefined,
       success: false,
+      dirty: {
+        category: false,
+        country: false,
+        captcha: false,
+      },
     }
   },
 
   methods: {
-    async signup () {
-      // check charter read
-      if (!this.charter) {
-        alert('Please read and accept the Terms of Service')
-        return
-      }
-      // check reCaptcha verified
-      if (!this.verified) {
-        alert('Please verify the captcha "I\'m not a robot"')
-        return
-      }
+    signup () {
+      this.$validator.validateAll().then(async (validated) => {
+        if (!validated || !this.user.country || !this.user.category) {
+          this.dirty.category = this.dirty.country = this.dirty.captcha = true
+          console.log('Form is not valid.')
+          return
+        }
+        // check reCaptcha verified
+        if (!this.captchaVerified) {
+          this.dirty.captcha = true
+          alert('Please verify the captcha "I\'m not a robot"')
+          return
+        }
 
-      try {
-        await iotlab.signup(this.user)
-        this.success = true
-      } catch (err) {
-        this.success = false
-        this.$notify({text: 'Error during signup', type: 'error'})
-      }
+        try {
+          await iotlab.signup(this.user)
+          this.success = true
+        } catch (err) {
+          this.success = false
+          this.$notify({text: 'Error during signup', type: 'error'})
+        }
+      })
     },
     onCaptchaVerify: function (response) {
       console.log('Captcha Verified')
-      this.verified = true
+      this.captchaVerified = true
     },
     onCaptchaExpired: function () {
       console.log('Captcha Expired')
-      this.verified = false
+      this.captchaVerified = false
     },
     userLabel (cat) {
       return UserCategories[cat]
+    },
+    toggleMotivations () {
+      $('#motivations').slideToggle()
     },
   },
 }
