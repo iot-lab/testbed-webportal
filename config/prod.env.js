bc7fe72611399441bc1c20bc851dca95c11120bd
Benoit Formet <benoit.formet@inria.fr>
2018-03-27 11:30:37 +0200

    new environment variable to control where the app is deployed (dev/dev-beta/prod/prod-beta)

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,3 @@
-if (!process.env.IOTLAB_HOST) {
-  process.env.IOTLAB_HOST = process.env.IOTLAB_ENV === 'prod' ? 'www.iot-lab.info' : 'devwww.iot-lab.info'
-}
-
 module.exports = {
   NODE_ENV: '"production"',
   IOTLAB_HOST: `"${process.env.IOTLAB_HOST}"`
