427f89047e93f05695e5c6a11c3802df68c2b111
Benoit Formet <benoit.formet@inria.fr>
2018-04-26 13:45:36 +0200

    allow to select nodes from the map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,36 +4,33 @@
   <h1>Map</h1>
   <ul class="nav nav-pills mb-2">
     <li class="nav-item" v-for="site in sites">
-      <a class="nav-link" :class="{'active': mapsite === site.site}" href="#" @click.prevent="mapsite = site.site; setMapSite()">{{site.site}}</a>
+      <a class="nav-link text-capitalize" :class="{'active': mapsite === site.site}" href="#" @click.prevent="mapsite = site.site">{{site.site}}</a>
     </li>
   </ul>
-  <div id="map" style="position: relative">
-    <div id="div3d" oncontextmenu="return false"></div>           
-    <div id="nodeInfo" class="p-2 text-white" style="position: absolute; top: 0"></div>
-    <div id="selectedInfo" class="my-2"></div>
+  <map-3d :nodes="mapNodes" @selected="setSelected"></map-3d>
+  <div class="my-2" v-if="selectedMapNodes.length">
+    <span style="vertical-align: middle">Selected nodes</span>
+    <span style="vertical-align: middle" class="badge badge-tag badge-primary" v-for="node in selectedMapNodes">{{node}}</span>
+    <button class="btn btn-sm btn-success"><i class="fa fa-plus"></i> Add to experiment</button>
+    <!-- <a href="" class="badge badge-tag badge-success"><i class="fa fa-plus"></i> Add to experiment</a> -->
   </div>
 </div> <!-- container -->
 </template>
         
 <script>
-import $ from 'jquery'
 import { iotlab } from '@/rest'
-// import { extractArchi } from '@/assets/js/iotlab-utils'
-import { loadNodes, init3d } from '@/assets/map3d/map'
-
-function newMapNode (node) {
-  // Expand node object returned by IoT-LAB API with computed attributes and functions
-  // return [node.network_address, node.x, node.y, node.z, extractArchi(node.archi), node.state]
-  return node
-}
+import Map3d from '@/components/parts/Map3d'
 
 export default {
   name: 'map',
+
+  components: { Map3d },
 
   data () {
     return {
       sites: [],
       nodes: [],
+      selectedMapNodes: [],
       mapsite: 'devgrenoble',
     }
   },
@@ -41,14 +38,17 @@ export default {
   async created () {
     this.sites = (await iotlab.getSites()).sort(site => site.site)
     this.nodes = await iotlab.getNodes()
-    this.setMapSite(this.mapsite)
+  },
+
+  computed: {
+    mapNodes () {
+      return this.nodes.filter(map => map.site === this.mapsite)
+    },
   },
 
   methods: {
-    setMapSite () {
-      $('#div3d').empty()
-      loadNodes(this.nodes.filter(map => map.site === this.mapsite).map(node => newMapNode(node)))
-      init3d()
+    setSelected (nodes) {
+      this.selectedMapNodes = nodes
     },
   },
 }
@@ -56,8 +56,4 @@ export default {
 
 <style scoped>
 
-#div3d {
-  height: 500px;
-  background: lightgray;
-}
 </style>
