19e0be581dcca758fc91c294560e45507ab6ce01
Benoit Formet <benoit.formet@inria.fr>
2018-07-19 09:45:41 +0200

    add new config

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+        moment: 'moment',
+        'window.moment': 'moment',
+      }),
+    ],
+  },
+  chainWebpack: config => {
+    // HTML Loader
+    config.module
+      .rule('html')
+      .test(/\.html$/)
+      .use('html-loader/loader')
+      .loader('html-loader')
+      .end()
+  },
+}
