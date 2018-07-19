d6c55c59645fb858ff9bee8211a3e3bcaeca0a3f
Benoit Formet <benoit.formet@inria.fr>
2018-07-19 13:14:29 +0200

    import fontawesome from node modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
