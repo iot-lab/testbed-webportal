87ebc293da1aae68ece531c66d75b436edaf558b
Benoit Formet <benoit.formet@inria.fr>
2018-09-19 14:11:31 +0200

    fab deploy to testbed-webportal folder (configurable)

--- fabfile.py
+++ fabfile.py
@@ -5,23 +5,20 @@ from fabric.api import local, run, put, lcd
 def test():
     """ Run Web portal tests. """
     local('npm run test:unit')
-    local('npm run test:e2e')
+    local('npm run test:e2e -- --headless')
     
 
-def build(env='dev-beta'):
+def build(env='dev'):
     """ Build Web portal (to ./dist). """
     local('npm run build:%s' % env)
     
-def deploy(dist="./dist"):
+def deploy(dist='./dist', dest='testbed-webportal'):
     """ Deploy Web portal. """
     with lcd(dist):
         run('rm -Rf /tmp/devportal/')
         run('mkdir /tmp/devportal')
         put("*", "/tmp/devportal/")
-    run('ssh srvwww \"rm -Rf /var/www/devportal/*\"')
-    run('ssh srvwww \"mkdir -p /var/www/devportal\"')
-    run('scp -r /tmp/devportal/* srvwww:/var/www/devportal/')
-    # run('ssh srvwww \"chown root:www-data /var/www/devportal/wp-menu/\"')
-    # run('ssh srvwww \"chmod g+w /var/www/devportal/wp-menu/\"')
-    # run('ssh srvwww \"rm /var/www/devportal/wp-menu/wp-menu.php\"')
+    run('ssh srvwww \"rm -Rf /var/www/%s/*\"' % dest)
+    run('ssh srvwww \"mkdir -p /var/www/%s\"' % dest)
+    run('scp -r /tmp/devportal/* srvwww:/var/www/%s/' % dest)
     run('rm -Rf /tmp/devportal/')
