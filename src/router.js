c96eaa44f7547e9984ce6ea6afacb1e7317f2377
Benoit Formet <benoit.formet@inria.fr>
2018-09-19 11:26:43 +0200

    switch router to history mode

--- src/router.js
+++ src/router.js
@@ -21,10 +21,10 @@ import {auth} from './auth'
 Vue.use(Router)
 
 const router = new Router({
-  // base: process.env.NODE_ENV === 'production'
-  //   ? process.env.VUE_APP_IOTLAB_PATH
-  //   : '/',
-  // mode: 'history',
+  base: process.env.NODE_ENV === 'production'
+    ? process.env.VUE_APP_IOTLAB_PATH
+    : '/',
+  mode: 'history',
   routes: [
     { path: '/', redirect: '/dashboard' },
     { path: '/drawgantt', name: 'drawgantt', component: Drawgantt },
