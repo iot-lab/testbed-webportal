9796e3fbc287cb237af0088baf113d9bbafc755f
Benoit Formet <benoit.formet@inria.fr>
2018-12-12 01:11:30 +0100

    multi cam config per site (draft)

---
+++ src/assets/map3d/SiteCameras.config.js
@@ -0,0 +1,26 @@
+// Categories for IoT-LAB users
+
+const SiteCameras = {
+  devgrenoble: [
+    {
+      name: 'Default',
+      camera: {x: 0, y: -75, z: 63},
+      origin: {x: 0, y: 0, z: 0},
+    },
+    {
+      name: 'My camera',
+      camera: {x: 12, y: 23, z: 12},
+      origin: {x: 0, y: 0, z: 0},
+    },
+    {
+      name: 'My camera 2',
+      camera: {x: -65, y: 37, z: 65},
+      origin: {x: -25, y: 6, z: 0},
+    },
+  ],
+  devlille: [],
+  devsaclay: [],
+  devstrasbourg: [],
+}
+
+export default SiteCameras
