cf9520ba5accacd283d1c7ef74f1cbbb48fd870c
rienafairefr <rienafairefr@gmail.com>
2019-04-05 13:26:26 +0200

    put the Drawgantt/Activity in the testbed toolbar

--- src/wp-menu/WordpressNavbar.vue
+++ src/wp-menu/WordpressNavbar.vue
@@ -12,9 +12,6 @@
       <nav class="navbar-collapse collapse navbar-ex1-collapse" role="navigation">
         <div v-html="topmenu"></div>
         <ul class="nav navbar-nav ml-auto">
-          <li class="nav-item" :class="{'active': $route.name === 'drawgantt'}">
-            <router-link :to="{name: 'drawgantt'}" title="Testbed Activity" class="nav-link"><i class="fa fa-fw fa-calendar"></i> Activity</router-link>
-          </li>
           <li class="nav-item" :class="{'active': $route.name !== 'drawgantt'}">
             <router-link :to="{name: 'dashboard'}" title="Testbed" class="nav-link"><i class="fa fa-wrench"></i> Testbed</router-link>
           </li>
