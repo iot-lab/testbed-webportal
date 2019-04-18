f1b09ccd230fe222e2b1212df3cf8c06e5c10454
rienafairefr <rienafairefr@gmail.com>
2019-04-18 15:08:17 +0200

    better model form

--- src/components/mobility/ModelForm.vue
+++ src/components/mobility/ModelForm.vue
@@ -14,11 +14,18 @@
           Invalid name. Only alphanumeric characters allowed [0-9A-Za-z_]
         </div>
       </div>
-      <div class="form-group">
+      <div class="form-group" v-if="!readOnly">
+        <label>Upload a Script file</label>
+        <input type="file" id="modelfile" ref="modelfile" name="modelfile" class="form-control" v-on:change="modelFileUpload">
+      </div>
+      <div class="form-group" v-if="!readOnly">
         <label>Model Script file</label>
         <input
           placeholder="file name"
           v-model="modelForm.script" class="form-control" type="text">
+      </div>
+      <div class="form-group">
+        <label>Model Script content</label>
         <editor
           v-model="modelFileForm.data"
           @init="editorInit"
@@ -65,9 +72,10 @@ export default {
         script: 'script.py',
       },
       modelFileForm: {
-        data: `# Here, you enter some calls to the robot API to make it move
+        data: `#!/usr/bin/env python
+# Here, you enter some calls to the robot API to make it move
 # You can look at the script for the \`random\` model for inspiration
-# the server is at robot:8081 
+# the server is at robot:8081
 # Content-type header should be application/json
 #
 # GET /robot/position
@@ -108,14 +116,30 @@ export default {
   },
 
   methods: {
-    editorInit: function () {
+    modelFileUpload () {
+      let reader = new FileReader()
+      reader.onload = (function (vm) {
+        return async function (e) {
+          vm.modelFileForm.data = e.target.result
+          console.log(e)
+          vm.modelForm.script = e.target.name
+        }
+      })(this)
+      reader.onerror = (function (vm) {
+        return async function (e) {
+          vm.$notify({text: 'error reading that file', type: 'error'})
+        }
+      })(this)
+      reader.readAsText(this.$refs.modelfile.files[0])
+    },
+    editorInit () {
       require('brace/ext/language_tools')
       require('brace/mode/python')
       require('brace/theme/chrome')
     },
     async updateMobilityModel () {
       let model = Object.assign({}, this.modelForm)
-      let modelFile = this.modelFileForm
+      let modelFile = this.modelFileForm.data
 
       if (!model.name) {
         this.$notify({text: 'Name is empty', type: 'warning'})
@@ -128,13 +152,13 @@ export default {
           // modify existing model
           console.log('modify model ' + this.mobilityModel.name)
           console.log(model)
-          console.log(modelFile.data)
-          await iotlab.updateMobilityModel(this.mobilityModel.name, model, modelFile.data)
+          console.log(modelFile)
+          await iotlab.updateMobilityModel(this.mobilityModel.name, model, modelFile)
         } else {
           console.log('create model ' + model.name)
           console.log(model)
           // create new model
-          await iotlab.createMobilityModel(model, modelFile.data)
+          await iotlab.createMobilityModel(model, modelFile)
         }
         this.$notify({ text: `model ${model.name} saved`, type: 'success' })
 
